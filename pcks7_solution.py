e8af81e235be3fa7e3cc2e8e9d21298aeaec683a
Mohan <notcoldum@gmail.com>
2022-02-10 02:01:33 -0500

    Decryption seems to work

--- pcks7_solution.py
+++ pcks7_solution.py
@@ -1,10 +1,4 @@
-# Here is a sample cookie:
-# 5468697320697320616e204956343536069242ad5ac3e289582b09ff2d30032b0e72a2004dc6d37181448f0327a2a3f3fe3280b99951c832ca8d08940716d226af1a2edddadfdbe92a5933f4d869c714e53842a369eb89a44ae1159b3b73f3d3
-# What is your cookie?
-
-from dataclasses import replace
 from pwn import *  # Import lib to use the APIs, just like any Python lib
-import re
 # Suppress non-error messages, i.e. less verbose runtime output
 context.log_level = 'error'
 context.proxy = (socks.SOCKS5, 'localhost', 8123)
@@ -13,10 +7,13 @@ context.proxy = (socks.SOCKS5, 'localhost', 8123)
 cookie = "5468697320697320616e204956343536069242ad5ac3e289582b09ff2d30032b0e72a2004dc6d37181448f0327a2a3f3fe3280b99951c832ca8d08940716d226af1a2edddadfdbe92a5933f4d869c714e53842a369eb89a44ae1159b3b73f3d3"
 plaintext = ""
 prev_xor_vals = []
+correct_guesses = []
 IV="This is an IV456"
 
 
 def attack_block(cipher, pre, post):
+    global plaintext    # removing this may cause "referenced before assignment error"
+
     # cipher contains 2 blocks, cipher[0:32] and cipher[32:]
     for pos in range(16):   # 16 bytes per block
         for xor_val in range(256):    # 0x00 to 0xFF
@@ -41,64 +38,65 @@ def attack_block(cipher, pre, post):
             for prev_pos in range(pos - 1, -1, -1):
                 replaced_byte = hex(
                     int(cipher[32 - 2 * (prev_pos + 1): 32 - 2 * prev_pos], 16)
-                    ^ prev_xor_vals[prev_pos]
+                    ^ correct_guesses[prev_pos]
                     ^ (pos + 1))[2:]
                 while len(replaced_byte) < 2:
                     replaced_byte = "0" + replaced_byte
                 message += replaced_byte
             
-            print("first half C(n-1) is: " + message)
+            # print("first half C(n-1) is: " + message)
 
             # append the second half
             message += cipher[32:]
 
-            # prepend / append message with the other 192 - 64 chars
-            # sending partial causes server failure in json.parse()
-            # message = pre + message + post
-
             # print(len(message)) # 192
             oracle.sendline(message)
             response = oracle.recvall()
-            # if pos == 1:
-            # print(response)
 
             if "padding" not in response.decode():
                 print(response)
-                print(message)
+                # if multiple xor_val's are valid, keep only the latest one
                 if pos + 1 == len(prev_xor_vals):
                     prev_xor_vals[pos] = xor_val
+                    correct_guesses[pos] = xor_val ^ (pos + 1)
                 else:
                     prev_xor_vals.append(xor_val)
+                    correct_guesses.append(xor_val ^ (pos + 1))
                 print(prev_xor_vals)
-                # plaintext = hex((pos + 1) ^ xor_val).split("0x")[1] + plaintext
-                # print(plaintext)
+                print(correct_guesses)
+                # plaintext_byte = hex((pos + 1) ^ xor_val).split("0x")[1]
+                # while len(plaintext_byte) < 2:
+                #     plaintext_byte = "0" + plaintext_byte
+                # plaintext = plaintext_byte + plaintext
+                plaintext = chr((pos + 1) ^ xor_val) + plaintext
+        print("New iteration: current plaintext is:" + plaintext)
 
-            # print(message)
-            # result = "00" * (16 - 1 - pos) + ("0" if len(xor_val) %
-            #                                   2 != 0 else "") + xor_val + c_n
 
 if __name__ == "__main__":
     # try decrypting the first block
+    # {"username": "gu
     IV_HEX = ""
     for ch in IV:
         ch_hex = hex(ord(ch))[2:]
         while len(ch_hex) < 2:
             ch_hex = "0" + ch_hex
         IV_HEX += ch_hex
-    print(IV_HEX + cookie[0:32])
-    print(len(IV_HEX + cookie[0:32]))
-    attack_block(IV_HEX + cookie[0:32], "", "")
+    # IV_HEX seems to be identical to cookie[0:64]
 
     # part 1: decryption attack
-    for blockIndex in range(6):
-        if blockIndex == 5:
-            print("first half now becomes IV. fix later")
-            break
+    # 0 - 64, 32 - 96, 64 - 128, 128 - 192
+    for blockIndex in range(5):
+        # if blockIndex == 5:
+        #     # first half now becomes IV
+        #     attack_block(IV_HEX + cookie[0:32], "", "")
+        #     break
         # 2-block segments = 64 chars = 32 bytes
         c = cookie[len(cookie) - 64 * (blockIndex + 1): len(cookie) - 64 * blockIndex]
         pre = cookie[0: len(cookie) - 64 * (blockIndex + 1)]
         post = cookie[len(cookie) - 64 * blockIndex :]
         attack_block(c, pre, post)
+    
+    print("Decryption done. Plaintext is: " + plaintext)
 
     # part 2: encryption attack
     # first get dec(c_n)
